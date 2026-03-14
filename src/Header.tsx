0740c51541e7aa0841c05a7167ceeb9b63868932
outta057 <outta057@gmail.com>
2026-03-14 21:06:46 +0200

    хедер 2

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,38 +1,91 @@
 import React from "react";
+import logo from "./assets/logo.png";
+
 
 const Header: React.FC = () => {
 	return (
 		<div className="header">
-			<div className="header__info">
-				<a href="" className="header__info__telephone">
-					<h5>+38 063 3333 333</h5>
-				</a>
+			<div className="header_info">
+				<div className="header_info_container">
+					<a href="" className="header_info_telephone">
+						<h5>+38 063 3333 333</h5>
+					</a>
 
-				<button className="header__info__call">Заказать звонок</button>
+					<button className="header_info_call">Заказать звонок</button>
 
-				<div className="header__info__work-hours">
-					<h5>с 09:00 до 21:00 без выходных</h5>
+					<div className="header_info_work-hours">
+						<h5>с 09:00 до 21:00 без выходных</h5>
+					</div>
 				</div>
 			</div>
-			<div className="header__main">
-				<div>
-					<p>лого</p>
-				</div>
-				<div>поиск</div>
-				<div>
-					<p>корзина</p>
+			<div className="header_main">
+				<div className="header_main_container">
+					<div className="header_main_container_logo">
+						<img src={logo} alt="logo" />
+					</div>
+					<div className="header_main_container_search">
+						<input type="text" placeholder="Поиск" />
+					</div>
+					<div className="header_main_block">
+						<div className="header_main_block_languages">
+						<div className="header_main_block_languages_div">
+							<button className="header_main_block_languages_button">UA</button>
+						</div>
+						<div className="header_main_block_languages_div">
+							<button className="header_main_block_languages_button">| EN |</button>
+						</div>
+						<div className="header_main_block_languages_div">
+							<button className="header_main_block_languages_button">RU</button>
+							</div>
+						</div>
+						<button className="header_main_block_button">обратная связь</button>
+						<a href="" className="header_main_block_ahref">
+							избранное
+						</a>
+						<button className="header_main_block_button">корзина</button>
+					</div>
 				</div>
 			</div>
-			<div className="header__categories">
-				<ul >
-					<li>Категория 1</li>
-					<li>Категория 2</li>
-					<li>Категория 3</li>
-					<li>Категория 4</li>
-					<li>Категория 5</li>
-					<li>Категория 6</li>
-					<li>Категория 7</li>
-				</ul>
+			<div className="header_categories">
+				<div className="header_categories_container">
+					<ul className="header_categories_container_ul">
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Все товары
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Женщинам
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Мужчинам
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Акции
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Размеры
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Доставка и оплата
+							</a>
+						</li>
+						<li>
+							<a href="" className="header_categories_container_ul_ahref">
+								Контакты
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
